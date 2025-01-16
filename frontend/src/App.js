0da93585d739ffad5762effb6794da97dde48235
Patrick Trainer <pat.trainer@gmail.com>
2025-01-15 22:04:39 -0600

    feat: add frontend structure and MotherDuck player

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import BinaryVideoPlayer from './MotherDuckVideoPlayer';
+
+function App() {
+  return (
+    <div className="App">
+      <BinaryVideoPlayer />
+    </div>
+  );
+}
+
+export default App;
